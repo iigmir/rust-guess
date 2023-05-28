59c5e073d2b41bd0a536126c3a3ec17139b5ae67
iigmir <roc120j@gmail.com>
2023-05-28 22:51:19 +0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-fn main()
+fn guess_num() -> String
 {
     println!("Guess the number!");
-
     println!("Please input your guess.");
-
-    let mut guess = String::new(); // mutable
-    // let apples = 5; // immutable
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
+    let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    guess.trim().to_string()
+}
+
+fn main()
+{
+    // let mut guess = String::new(); // mutable
+    // let apples = 5; // immutable
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let guess = guess_num();
+    println!("You guessed: {}", guess);
 }
