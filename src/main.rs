f3c899d5a50da0946f2cd3c860f6d4425939029d
iigmir <roc120j@gmail.com>
2023-05-28 23:21:22 +0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,18 @@ fn guess_num() -> u64
 
     let guess: u64 = match guess.trim().parse() {
         Ok(num) => num,
-        Err(_) => -1,
+        Err(_) => 0,
     };
     return guess
+}
+
+fn comparing_number(secret_number: u64, guessing_number: u64) -> String
+{
+    match guessing_number.cmp(&secret_number) {
+        Ordering::Less => "Too small!".to_string(),
+        Ordering::Greater => "Too big!".to_string(),
+        Ordering::Equal => "You win!".to_string()
+    }
 }
 
 fn main()
@@ -25,13 +34,10 @@ fn main()
     loop {
         let guess = guess_num();
         println!("You guessed: {}", guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+        let message = comparing_number( secret_number, guess );
+        println!( "{}", message );
+        if guess == secret_number {
+            break;
         }
     }
 }
